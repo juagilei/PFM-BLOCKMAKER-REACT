dd2cc7666f2d03e251ee01fe19ed1fb1d09a70f1
juagilei <juagilei@gmail.com>
2024-02-26 13:03:25 +0100

    AppLayout final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import { Home, Altas, Operaciones, Info, NotFound } from "./pages";
+import AppLayout from "./components/ui/Layouts/AppLayout";
 export default function App() {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="Altas" element={<Altas />} />
-      <Route path="Operaciones" element={<Operaciones />} />
-      <Route path="Info" element={<Info />} />
 
-      {/* Using path="*"" means "match anything", so this route
-        acts like a catch-all for URLs that we don't have explicit
-        routes for. */}
-      <Route path="*" element={<NotFound />} />
+    <AppLayout>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="Altas" element={<Altas />} />
+        <Route path="Operaciones" element={<Operaciones />} />
+        <Route path="Info" element={<Info />} />
+
+        {/* Using path="*"" means "match anything", so this route
+          acts like a catch-all for URLs that we don't have explicit
+          routes for. */}
+        <Route path="*" element={<NotFound />} />
     </Routes>
+    </AppLayout>
   );
 }
