92de29f2e643985b7ea87abd1061bae2080269fe
juagilei <juagilei@gmail.com>
2024-02-27 22:39:01 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return (
     <footer className="bg-black py-4 px-10 sm:p-8 flex justify-between items-center text-gray-300">
-            <p>Derechos de autor &copy; {new Date().getFullYear()} Blockmaker Academy</p>
+            <p>Derechos de autor &copy; {new Date().getFullYear()} Juagilei</p>
             <p>Contacto: <a href="mailto:juagilei@gmail.com">juagilei@gmail.com</a></p>
              <a href="https://github.com/juagilei/PFM-BLOCKMAKER-REACT.git">
             <img src="../../public/github-mark-white.png" alt="Icono git" width={50} />
