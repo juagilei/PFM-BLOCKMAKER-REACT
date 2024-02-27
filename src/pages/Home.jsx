2931877a361d78243d329e22d7cb936226c7a203
juagilei <juagilei@gmail.com>
2024-02-27 22:15:39 +0100

    restore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className=" text-red-700 text-2xl">
-      <h1>Esto es el Home</h1>
+    <div className="bg-black min-h-[85vh]">
+      <img src="../../public/bola completo.png" alt="Imagen bola" className=" hidden md:flex w-auto h-auto" />
+      <img src="../../public/sin bola.png" alt="Imagen sin bola" className=" w-auto h-auto md:hidden"/>
     </div>
   );
 }
