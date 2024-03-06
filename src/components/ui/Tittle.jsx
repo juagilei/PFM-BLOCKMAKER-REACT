93b704c26d9b5cb6fecff5f17096597dc05c4b1c
juagilei <juagilei@gmail.com>
2024-03-06 18:44:50 +0100

    modificar Tittle

--- src/components/ui/Tittle.jsx
+++ src/components/ui/Tittle.jsx
@@ -1,10 +1,12 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-export default function Tittle({children}) {
-    return (
-        <div className="font-bold text-white text-lg mb-2 w-fit"><h3>{ children}</h3></div>
-    )
+export default function Tittle({ children }) {
+  return (
+    <div className="font-bold text-2xl mb-2 w-fit bg-gradient-to-r from-fuchsia-700 to-pink-400 text-transparent bg-clip-text">
+      <h3>{children}</h3>
+    </div>
+  );
 }
-Tittle.propsTypes = {
-    children: PropTypes.string
-}
+Tittle.propTypes = {
+  children: PropTypes.string,
+};
