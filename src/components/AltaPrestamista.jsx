05711f8a995a1c153ec5769f0b81fcb3bcfd4569
juagilei <juagilei@gmail.com>
2024-03-06 21:10:42 +0100

    Diseño AltaPrestamista Boton

--- src/components/AltaPrestamista.jsx
+++ src/components/AltaPrestamista.jsx
@@ -1,8 +1,23 @@
-import {Tittle} from '../components/ui'
+import { Tittle, TextInput, Button } from '../components/ui'
 export default function AltaPrestamista() {
     return (
-        <Tittle>
-            Alta Prestamista
-        </Tittle>
+        <div className='border shadow rounded-xl p-5 border-fuchsia-600'>
+            <Tittle>
+                Alta Prestamista
+            </Tittle>
+        
+            <form>
+                    <div className=' flex flex-col justify-center gap-3'>
+                        <h2> Introduce la direccion del nuevo prestamista</h2>
+                        <TextInput
+                            type="text"
+                            placeholder="Address"
+                        />
+                        <Button>
+                            Dar de alta
+                        </Button>
+                    </div>
+            </form>
+        </div>
     )
 }
