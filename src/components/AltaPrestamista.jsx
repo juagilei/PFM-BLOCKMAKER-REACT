507e41976c51d88bbb1eb0af62d1fd2ac79cf4a2
juagilei <juagilei@gmail.com>
2024-03-06 22:15:58 +0100

    ajuste texto

--- src/components/AltaPrestamista.jsx
+++ src/components/AltaPrestamista.jsx
@@ -1,23 +1,16 @@
-import { Tittle, TextInput, Button } from '../components/ui'
+import { Tittle, TextInput, Button } from "../components/ui";
 export default function AltaPrestamista() {
-    return (
-        <div className='border shadow rounded-xl p-5 border-fuchsia-600'>
-            <Tittle>
-                Alta Prestamista
-            </Tittle>
-        
-            <form>
-                    <div className=' flex flex-col justify-center gap-3'>
-                        <h2> Introduce la direccion del nuevo prestamista</h2>
-                        <TextInput
-                            type="text"
-                            placeholder="Address"
-                        />
-                        <Button>
-                            Dar de alta
-                        </Button>
-                    </div>
-            </form>
+  return (
+    <div className="border shadow rounded-xl p-5 border-fuchsia-600">
+      <Tittle>Alta Prestamista</Tittle>
+
+      <form>
+        <div className=" flex flex-col justify-center gap-3">
+          <h2> Introduce la direccion del nuevo prestamista</h2>
+          <TextInput type="text" placeholder="Address" />
+          <Button>Dar de alta</Button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
