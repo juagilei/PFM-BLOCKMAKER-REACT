653e86b5fac0fb364c65e53df0a9d79e635b505f
juagilei <juagilei@gmail.com>
2024-03-06 23:28:29 +0100

    input AltaPrestamista

--- src/components/AltaPrestamista.jsx
+++ src/components/AltaPrestamista.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { Tittle, TextInput, Button } from "../components/ui";
 export default function AltaPrestamista() {
+    const { address, setAddress } = useState()
+    {/* creo una variable para manejar el dat que introducimos en el input como address */ }
+
+    const handlerAddressInputChange = ((event) => {
+        { /* En este caso el valor de address se aplica con setAddress y que guardado en address para eso se usa el useState  */ }
+        setAddress(event.target.value)
+                      })
   return (
-    <div className="border shadow rounded-xl p-5 border-fuchsia-600">
+    <section className="flex flex-col justify-center border shadow rounded-xl p-5 border-fuchsia-600">
       <Tittle>Alta Prestamista</Tittle>
 
       <form>
         <div className=" flex flex-col justify-center gap-3">
-          <h2> Introduce la direccion del nuevo prestamista</h2>
-          <TextInput type="text" placeholder="Address" />
-          <Button>Dar de alta</Button>
+          <h2> Introduce la direccion del nuevo prestamista </h2>
+                  <TextInput
+                      type="text"
+                      placeholder="Address"
+                      onChange={handlerAddressInputChange}
+                  />
+          <Button>Dar de alta </Button>
         </div>
-      </form>
-    </div>
+        </form>
+    </section>
   );
 }
