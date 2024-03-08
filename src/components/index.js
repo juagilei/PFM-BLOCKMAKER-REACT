5394fde209d0438c3f264ec23b9b2aabb34d6e08
juagilei <juagilei@gmail.com>
2024-03-08 09:37:28 +0100

    creo componente alta cliente

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ export { default as NavBar } from "./NavBar";
 export { default as Navigation } from "./Navigation";
 export { default as AltaPrestamistaCliente } from "./AltaPrestamistaCliente";
 export { default as AltaPrestamista } from "./AltaPrestamista";
+export { default as AltaCliente } from "./AltaCkliente";
 
 
