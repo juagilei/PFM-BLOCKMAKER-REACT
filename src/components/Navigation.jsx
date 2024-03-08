7782a6c729c70485983a22d90286189c0e8290a5
juagilei <juagilei@gmail.com>
2024-03-08 11:43:27 +0100

    ajuste pantalla

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,25 +6,25 @@ export default function Navigation() {
     <div className="ml-4 flex items-center">
       <Link
         to="/"
-        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-xl font-medium"
+        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-lg font-medium"
       >
         Home
       </Link>
       <Link
         to="/Altas"
-        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-xl font-medium"
+        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-lg font-medium"
       >
         Altas
       </Link>
       <Link
         to="/Operaciones"
-        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-xl font-medium"
+        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-lg font-medium"
       >
         Operaciones
       </Link>
       <Link
         to="/Info"
-        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-xl font-medium"
+        className="text-fuchsia-700 hover:bg-gray-700 hover:text-pink-500 rounded-md px-3 py-2 text-lg font-medium"
       >
         Info
       </Link>
