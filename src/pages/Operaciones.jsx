4e73d6144c909a7598860dbe39f0f02231e28072
juagilei <juagilei@gmail.com>
2024-03-08 15:55:46 +0100

    diseño pagina operaciones

--- src/pages/Operaciones.jsx
+++ src/pages/Operaciones.jsx
@@ -1,5 +1,17 @@
-export default function Operaciopnes() {
-    return (
-        <h1>Esta es la pagina de operaciones </h1>
-    )
+import { useAccount } from "wagmi";
+import AltaPrestamistaCliente from "../components/AltaPrestamistaCliente";
+
+
+export default function Operaciones() {
+  const {isConnected } = useAccount();
+  return (
+    <div className=" mx-auto min-h-[85vh] flex justify-center items-center text-white">
+      {isConnected ? (
+        <AltaPrestamistaCliente />
+      ) : (
+         <h1 className="text-2xl text-gray-400 ">🔒 Conecta tu wallet para comenzar </h1>
+      )}
+    </div>
+
+  );
 }
