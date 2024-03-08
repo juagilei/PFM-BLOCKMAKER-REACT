eec562699e87858f01155d48f74d642c923c54d9
juagilei <juagilei@gmail.com>
2024-03-08 12:12:04 +0100

    manejo de errores ALtaPrestamista

--- src/components/AltaPrestamista.jsx
+++ src/components/AltaPrestamista.jsx
@@ -1,36 +1,32 @@
 import { useState } from "react";
 import { Tittle, TextInput, Button } from "../components/ui";
-import { useWriteContract } from "wagmi";
+import { useWriteContract } from "wagmi"; /
 import { PrestamoDefiABI } from "../contracts/ABIs";
 
 export default function AltaPrestamista() {
-  {
-    /* ojo al declarar las variables del useState son un array asi que va entre [] */
-  }
   const [address, setAddress] = useState("");
-  {
-    /* creo una variable para manejar el dat que introducimos en el input como address */
-  }
 
   const handlerAddressInputChange = (event) => {
-    {
-      /* En este caso el valor de address se aplica con setAddress y que guardado en address para eso se usa el useState  */
-    }
     setAddress(event.target.value);
   };
 
-  {
-    /* Usamos el hook useWriteContract para escribir en el contrato */
-  }
-  const { writeContract } = useWriteContract();
+  const { writeContract } = useWriteContract(); 
 
-  const handlerOnClick = () =>
+  const handlerOnClick = () => {
+    
     writeContract({
       abi: PrestamoDefiABI,
       address: import.meta.env.VITE_PRESTAMO_DEFI_CONTRACT_ADDRESS,
-      functionName: "altaPrestamista (0xc1e83336)",
+      functionName: "altaPrestamista", 
       args: [address],
+    }).then((response) => {
+      // Maneja la respuesta de la transacción
+      console.log("Transaction response:", response);
+    }).catch((error) => {
+      // Maneja los errores de la transacción
+      console.error("Transaction error:", error);
     });
+  };
 
   return (
     <section className="flex flex-col justify-center border shadow rounded-xl p-5 border-fuchsia-600">
@@ -38,7 +34,7 @@ export default function AltaPrestamista() {
 
       <form>
         <div className=" flex flex-col justify-center gap-3">
-          <h2> Introduce la direccion del nuevo prestamista </h2>
+          <h2> Introduce la dirección del nuevo prestamista </h2>
           <TextInput
             type="text"
             placeholder="Address"
